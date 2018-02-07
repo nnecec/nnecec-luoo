9bbcb7e04d34afe12dde5daf776d6ecbc0798e50
nnecec <shine_c@outlook.com>
2018-02-07 21:18:28 +0800

    make concise

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
-
-
-import "./stylesheets/main.css";
-
-// Small helpers you might want to keep
-import "./helpers/context_menu.js";
-import "./helpers/external_links.js";
-
-import { remote } from "electron";
-import jetpack from "fs-jetpack";
-import env from "env";
 import React from 'react'
 import { render } from 'react-dom'
 
+import "./styles/main.css";
 
 render(
   <h1>Hello, world!</h1>,
