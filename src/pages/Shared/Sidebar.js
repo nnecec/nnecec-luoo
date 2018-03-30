f3b4555a8aae1ef1d820dd1cca5bbd03b0e061eb
nnecec <nnecec@outlook.com>
2018-03-30 19:27:22 +0800

    add animejs

--- src/pages/Shared/Sidebar.js
+++ src/pages/Shared/Sidebar.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import anime from 'animejs'
 
 export default class Sidebar extends Component {
+
+  componentDidMount() {
+
+  }
+
+  handleExpand = () => {
+    
+  }
+
   render() {
     return (
-      <div className="navbar luoo-header">
+      <div className="navbar luoo-sidebar" ref={r => this.sidebar = r}>
         <nav className="link">
           <Link to="/">主页</Link>
           <Link to="/music/tag/1">期刊</Link>
           <Link to="/musician">单曲</Link>
           <Link to="/essay">专栏</Link>
         </nav>
-
       </div>
     );
   }
